350a199c89230b652c336303e74241769b403219
Devendra Johari <devendrajohari9@gmail.com>
2021-02-14 10:08:27 +0530

    third commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-
+console.log("Cross site JavaScript Loaded!");
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -16,16 +16,16 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
 
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-      response.json().then((data) => {
-        if(data.error){
-          messageOne.textContent = data.error
-        }else{
-          messageOne.textContent = 'Location : ' + data.location
-          messageTwo.textContent = 'Weather : ' + data.forecast.weather_description +  ". It is currently " + data.forecast.temperature + ' degress out. It feels like ' + data.forecast.feelslike + ' degree out'
-          console.log(data.forecast);
-        }
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = 'Location : ' + data.location
+                messageTwo.textContent = 'Weather : ' + data.forecast.weather_description + ". It is currently " + data.forecast.temperature + ' degress out. It feels like ' + data.forecast.feelslike + ' degree out'
+                console.log(data.forecast);
+            }
 
-      })
+        })
     })
 
 })
